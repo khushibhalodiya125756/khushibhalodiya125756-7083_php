dda9d0e3013a66537b2d98c9428d5b6be0a6b5fd
khushibhalodiya125756 <bhalodiyakhushi07@gmail.com>
2026-09-08 07:24:47 +0530

    Add files via upload

---
+++ UNIT 1/P2.php
@@ -0,0 +1,15 @@
+<?php
+$a = 20;
+$b = 45;
+
+if ($a > $b)
+{
+    echo "Maximum Number = " . $a . "<br>";
+    echo "Minimum Number = " . $b;
+}
+else
+{
+    echo "Maximum Number = " . $b . "<br>";
+    echo "Minimum Number = " . $a;
+}
+?>
