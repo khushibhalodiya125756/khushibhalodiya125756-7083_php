9039d09847c8672e0c3f4bc46836c1484f152801
khushibhalodiya125756 <bhalodiyakhushi07@gmail.com>
2026-09-08 07:27:18 +0530

    Add files via upload

---
+++ UNIT 1/P1.php
@@ -0,0 +1,17 @@
+<?php
+// Constant
+define("COLLEGE", "ABC College");
+
+// Variables
+$name = "Vaibhav";
+$semester = "Semester 2";
+$percentage = 78.50;
+$result = "PASS";
+
+echo "<h2>Previous Semester Result</h2>";
+echo "College Name: " . COLLEGE . "<br>";
+echo "Student Name: " . $name . "<br>";
+echo "Semester: " . $semester . "<br>";
+echo "Percentage: " . $percentage . "%<br>";
+echo "Result: " . $result;
+?>
